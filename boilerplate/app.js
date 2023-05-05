b59fc747cbc5083c301ce37b480cd0a76d01169b
Parth Gupta <guptaparth1911@gmail.com>
2023-05-05 20:57:38 +0530

    capital W

--- boilerplate/app.js
+++ boilerplate/app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === "teach") {
-    console.log(` this log is for each request \n on the app.js page here we declare middleware(response handler is the last middleware using app.use where app=express()) which are run when we send the request`);
+    console.log(` this log is for each request \n on the app.js page here we declare middleware(response handler is the last middleware using app.use where app=express()) Which are run when we send the request`);
   }
   next();
 });
