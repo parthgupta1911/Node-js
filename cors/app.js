7ec21a9cf8ed8244c373d61ffd0c8a894be36b14
parthgupta1911 <guptaparth1911@gmail.com>
2023-05-09 12:02:35 +0530

    added cors

---
+++ cors/app.js
@@ -0,0 +1,15 @@
+const app = require("express")();
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "teach") {
+    console.log(` this log is for each request \n on the app.js page here we declare middleware(response handler is the last middleware using app.use where app=express()) Which are run when we send the request`);
+  }
+  next();
+});
+app.use((req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "abhi k liya itna hi",
+  });
+});
+module.exports = app;
